2957b63fefe4d300a04d225f841a3632256b9035
Dmitry <demosne@gmail.com>
2025-04-01 14:49:11 +0300

    feat: implement file renaming functionality with error handling

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+
 const rename = async () => {
-    // Write your code here 
+  const path = `${process.cwd()}/src/fs/files/wrongFilename.txt`;
+  const targetPath = `${process.cwd()}/src/fs/files/properFilename.md`;
+
+  if (!fs.existsSync(path) || fs.existsSync(targetPath)) {
+    console.error("FS operation failed");
+  } else {
+    fs.rename(path, targetPath, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("renamed");
+      }
+    });
+  }
 };
 
 await rename();
