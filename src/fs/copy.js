b5340c81ee6e914fba97aeb87c1146af7e9e1af3
Dmitry <demosne@gmail.com>
2025-04-01 14:14:14 +0300

    feat: implement file copy functionality with error handling

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+
 const copy = async () => {
-    // Write your code here 
+  const path = `${process.cwd()}/src/fs/files`;
+  const targetPath = `${process.cwd()}/src/fs/files_copy`;
+
+  if (fs.existsSync(targetPath) || !fs.existsSync(path)) {
+    console.error("FS operation failed");
+  } else {
+    fs.cp(path, targetPath, { recursive: true }, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("copyed");
+      }
+    });
+  }
 };
 
 await copy();
