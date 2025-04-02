60b74974c9fd4e2c911cedc8592e13aa0ca20cfd
Dmitry <demosne@gmail.com>
2025-04-02 14:44:22 +0300

    feat: implement argument parsing and logging for command line inputs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const buffer = [];
+
+  for (let i = 0; i < args.length; i++) {
+    const el = args[i];
+    if (el.startsWith("--")) {
+      buffer.push(`${el.slice(2)} is ${args[i + 1]}`);
+    }
+  }
+
+  if (args.length === 0) {
+    console.log("There are no args passed");
+    return;
+  }
+
+  console.log(buffer.join("; "));
 };
 
 parseArgs();
