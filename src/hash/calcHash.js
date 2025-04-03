5146f4401eeb8ea15fbcc3972966bc4cb3f03ecf
Dmitry <demosne@gmail.com>
2025-04-03 11:06:34 +0300

    feat: implement SHA256 hash calculation for a specified file with error handling

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,37 @@
+import fs from "fs";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  return new Promise((resolve, reject) => {
+    const path = `${process.cwd()}/src/hash/files/fileToCalculateHashFor.txt`;
+
+    const fileStream = fs.createReadStream(path);
+    const hashStream = crypto.createHash("sha256");
+    hashStream.setEncoding("hex");
+
+    fileStream.on("error", (err) => {
+      console.error(`Error reading file: ${path}`, err);
+      reject(err);
+    });
+
+    hashStream.on("error", (err) => {
+      console.error(`Error during hashing: ${path}`, err);
+      reject(err);
+    });
+
+    fileStream.pipe(hashStream);
+
+    hashStream.on("finish", () => {
+      try {
+        const hexHash = hashStream.read();
+        console.log(`SHA256 Hash: ${hexHash}`);
+        resolve();
+      } catch (err) {
+        console.error(`Error finalizing hash: `, err);
+        reject(err);
+      }
+    });
+  });
 };
 
 await calculateHash();
