5bb11afda01c4adce1e20378797dc78cb76244a0
Dmitry <demosne@gmail.com>
2025-04-04 14:54:44 +0300

    feat: implement file reading functionality with streaming and output to stdout

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import { stdout } from "process";
+
 const read = async () => {
-    // Write your code here 
+  const path = `${process.cwd()}/src/streams/files/fileToRead.txt`;
+  const readStream = fs.createReadStream(path);
+  const chunks = [];
+
+  readStream.on("data", (chunk) => {
+    chunks.push(chunk);
+  });
+
+  readStream.on("end", () => {
+    const data = Buffer.concat(chunks).toString();
+    stdout.write(data + "\n");
+    readStream.close();
+  });
 };
 
 await read();
